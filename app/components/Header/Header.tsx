80206429efcdf0392fc0b64a59531057570dc151
rudio1 <guilhermerudio6@gmail.com>
2025-07-02 14:43:36 -0300

    feat: implenting desktop header

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,17 +1,14 @@
 import Link from 'next/link';
 import styles from './Header.module.scss';
 
-interface HeaderProps {
-  title: string;
-}
-
-export default function Header({ title }: HeaderProps) {
+export default function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>{title}</h1>
       <nav className={styles.nav}>
         <Link href="/" className={styles.navLink}>Início</Link>
-        <Link href="/sobre" className={styles.navLink}>Sobre</Link>
+        <Link href="/sobre" className={styles.navLink}>Sobre Mim</Link>
+        <Link href="/servicos" className={styles.navLink}>Serviços</Link>
+        <Link href="/projetos" className={styles.navLink}>Projetos</Link>
         <Link href="/contato" className={styles.navLink}>Contato</Link>
       </nav>
     </header>
